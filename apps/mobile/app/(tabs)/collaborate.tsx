f303f6f14f7c8e931a676368a05b2e849c778066
tbmobb813 <nixjason40@gmail.com>
2025-09-25 00:38:49 -0400

    chore: prettier jsxSingleQuote + apply lint:fix and format

--- apps/mobile/app/(tabs)/collaborate.tsx
+++ apps/mobile/app/(tabs)/collaborate.tsx
@@ -44,7 +44,7 @@ export default function CollaborateScreen() {
     <View style={styles.container}>
       <CollaborativeEditor
         roomId={roomId}
-        documentId="main-document"
+        documentId='main-document'
         userId={userId}
         userName={userName}
         onContentChange={handleContentChange}
