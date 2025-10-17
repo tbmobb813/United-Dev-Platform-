875c14367d43f91ea9d677d035ee68af8bc833c6
tbmobb813 <nixjason40@gmail.com>
2025-10-17 19:49:21 -0400

    test(config): enable jest env in config package test

--- packages/config/__tests__/core/config.test.ts
+++ packages/config/__tests__/core/config.test.ts
@@ -1,5 +1,10 @@
 describe('packages/config - sanity', () => {
   it('basic arithmetic sanity check', () => {
     expect(1 + 1).toBe(2);
+  /* eslint-env jest */
+
+  describe('packages/config - sanity', () => {
+    it('basic arithmetic sanity check', () => {
+      expect(1 + 1).toBe(2);
+    });
   });
-});
