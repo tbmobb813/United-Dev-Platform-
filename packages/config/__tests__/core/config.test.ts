02cc521b622f799ce6a21aabf434d553dc3a913a
tbmobb813 <nixjason40@gmail.com>
2025-10-17 20:15:53 -0400

    test(config): fix malformed test and enable jest env

--- packages/config/__tests__/core/config.test.ts
+++ packages/config/__tests__/core/config.test.ts
@@ -1,10 +1,7 @@
+/* eslint-env jest */
+
 describe('packages/config - sanity', () => {
   it('basic arithmetic sanity check', () => {
     expect(1 + 1).toBe(2);
-  /* eslint-env jest */
-
-  describe('packages/config - sanity', () => {
-    it('basic arithmetic sanity check', () => {
-      expect(1 + 1).toBe(2);
-    });
   });
+});
