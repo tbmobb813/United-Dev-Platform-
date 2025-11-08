5af8943151210c78720939d68b1ed6d973d31f6c
tbmobb813 <jsn.nix@gmail.com>
2025-11-07 22:31:27 -0500

    chore: resolve merge conflicts (accept ours)

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
-const ClientIndex = dynamic(() => import('./index.client'), { ssr: false });
+const Client = dynamic(() => import('./index.client'), { ssr: false });
 
-export default function IndexWrapper() {
-  return <ClientIndex />;
+export default function Home() {
+  return <Client />;
 }
