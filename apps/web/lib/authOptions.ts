bb0e93d1f5497129d2b96ecb593088fecef05aa8
tbmobb813 <jsn.nix@gmail.com>
2025-11-09 10:49:54 -0500

    refactor: update Prisma import style for consistency and clarity

--- apps/web/lib/authOptions.ts
+++ apps/web/lib/authOptions.ts
@@ -1,9 +1,9 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import * as Prisma from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import type { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
-const prisma = new Prisma.PrismaClient();
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -1,9 +1,9 @@
-import * as Prisma from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: Prisma.PrismaClient | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new Prisma.PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
